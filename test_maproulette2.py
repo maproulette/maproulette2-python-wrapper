b03df0103159f510056180db4b361d57a6715c39
Martijn van Exel <m@rtijn.org>
2016-10-25 16:48:28 -0600

    adding initial tests

--- test_maproulette2.py
+++ test_maproulette2.py
@@ -4,4 +4,31 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import maproulette2
+from maproulette2 import *
+
+import unittest
+
+
+class TestApi(unittest.TestCase):
+    """Unit tests for the Maproulette 2 API wrapper"""
+
+    def setUp(self):
+        api_key = os.environ["MR_API_KEY"]
+        self.server = Server(api_key)
+
+    def tearDown(self):
+        pass
+
+    def test_createProject(self):
+        """Test creation of a project locally"""
+        p = Project()
+        self.assertIsInstance(p, Project)
+
+    def test_retrieveProject(self):
+        """Test retrieving a project from the server"""
+        p = Project(1)
+        p.get(self.server)
+        self.assertIn('"id": 1,', p.__str__())
+
+if __name__ == '__main__':
+    unittest.main()
