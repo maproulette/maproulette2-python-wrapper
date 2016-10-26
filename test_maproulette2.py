57c12c97131d93f0405c53fbbfb86316276129e5
Martijn van Exel <m@rtijn.org>
2016-10-26 10:20:07 -0600

    adding more tests

--- test_maproulette2.py
+++ test_maproulette2.py
@@ -17,18 +17,36 @@ class TestApi(unittest.TestCase):
         self.server = Server(api_key)
 
     def tearDown(self):
-        pass
+        self.server = None
 
     def test_createProject(self):
         """Test creation of a project locally"""
         p = Project()
         self.assertIsInstance(p, Project)
+        p = Project(1234)
+        self.assertEqual(p.id, 1234)
+        self.assertIsNone(p.name)
+        self.assertIsNone(p.description)
+
+    def test_modifyProjectName(self):
+        p = Project()
+        p.name = "New Project Name"
+        self.assertEqual(p.name, "New Project Name")
+
+    def test_modifyProjectId(self):
+        """Tests trying to change the Project ID, which should not be possible as it is set by MapRoulette"""
+        p = Project(1234)
+        with self.assertRaises(Exception):
+            p.id = 12345
 
     def test_retrieveProject(self):
         """Test retrieving a project from the server"""
         p = Project(1)
         p.get(self.server)
-        self.assertIn('"id": 1,', p.__str__())
+        self.assertIn('"id": 1', p.__str__())
 
 if __name__ == '__main__':
-    unittest.main()
+    if os.environ.get("MR_API_KEY"):
+        unittest.main()
+    else:
+        print("Please set your MapRoulette API key in environment variable MR_API_KEY first.")
